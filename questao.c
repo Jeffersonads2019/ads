731b26aa6d197a46add38cce3741db17df992cfc
Jeffersonads2019 <54963133+Jeffersonads2019@users.noreply.github.com>
2019-09-05 19:03:27 -0300

    Add files via upload

---
+++ questao.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main (){
+int valor1, valor2;
+scanf("%i%i",&valor1,&valor2);
+if(valor1>valor2){
+printf("valor1 é maior");
+}else{
+if ("valor<valor2"){
+printf("valor2 é maior");
+}else{
+printf ("numeros iguais");
+}
+    }
+}
